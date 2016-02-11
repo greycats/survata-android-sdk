0f1aad6bdc1ce3463648260a2e667313281d69c8
evan <liusuzheng2012@gmail.com>
2016-02-11 16:45:22 +0800

    Initial Commit

---
+++ survata-sample/src/main/java/com/survata/demo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.survata.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mCreateSurvey;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCreateSurvey = (Button) findViewById(R.id.create_survey);
+        mCreateSurvey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+    }
+}
